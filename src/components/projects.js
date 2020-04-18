571b7772d13ca37b46405bd794aa0c4732ad752d
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-04-18 22:58:47 +1200

    Deploying, changing resume and projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import BitmojiConstruction from '../images/bitmojiConstruction.png';
 
 class Projects extends React.Component{
     render(){
         return (
-            <div className ="in-construction">
-                <p className = "in-text">Page under construction, check back soon!</p>
-                <img src = {BitmojiConstruction} className = "bitmoji-construction-projects"/>
+            <div>
+                
             </div>
         )
     }
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BitmojiConstruction from '../images/bitmojiConstruction.png';
 
 class Resume extends React.Component{
     render(){
         return (
-            <div>
-                
+            <div className ="in-construction">
+                <p className = "in-text">Page under construction, check back soon!</p>
+                <img src = {BitmojiConstruction} className = "bitmoji-construction-projects"/>
             </div>
         )
     }
