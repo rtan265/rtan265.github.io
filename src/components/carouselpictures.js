ad1b9dd972c88ad8734236ae78d1a9385395dd67
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-04-20 02:59:21 +1200

    Made images for personal/work in projects. Added images to respective card backgrounds. Minor style changes for projects.js

---
+++ src/components/carouselpictures.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CarouselPictures extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <p>Hello!</p>
+            </div>
+        )
+    }
+}
+
+export default CarouselPictures;
