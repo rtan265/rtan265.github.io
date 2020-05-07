de907b823865c99e8d35b4ebb4029867593621b1
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-07 17:18:57 +1200

    added comment to project.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Tabs, Tab, Card, CardTitle, CardActions, Button, CardMenu, CardText } from 'react-mdl';
 import { Container, Col, Row } from 'react-bootstrap';
 
+// Add new Project: Learnfroma.kiwi
+
 
 class Projects extends React.Component {
     constructor(props) {
