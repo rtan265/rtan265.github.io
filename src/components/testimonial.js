ee988e006fe6d90f9f836d14a539c6980ac59344
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-22 21:43:54 +1200

    Adding base template to testimonials.js

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 
 class Testimonials extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                Testimonials.js working! 
+            </div>
+        )
+    }
 
 }
 
