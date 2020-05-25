2e3c64769c6ba40ce3cd72c21a24f7aab422f8e8
Ryan Tan <rtan265@aucklanduni.ac.nz>
2020-05-25 18:04:49 +1200

    added newProjectsPage and ProjectCards

---
+++ src/components/newProjectPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProjectCards from './projectCards';
+
+class Projects extends React.Component{
+    render() {
+        return (
+            <div>
+                <ProjectCards />
+            </div>
+        )
+    }
+}
+
+export default Projects
---
+++ src/components/projectCards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Tabs, Tab, Card, CardTitle, CardActions, Button, CardMenu, CardText } from 'react-mdl';
+import { Container, Col, Row } from 'react-bootstrap';
+
+class ProjectCards extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>project cards</div>
+        )
+    }
+}
+
+export default ProjectCards
