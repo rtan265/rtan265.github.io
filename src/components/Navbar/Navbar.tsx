5fe5029554e6e2ccb45108092f0aa26d99e27252
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-10-27 16:55:45 +1300

    Add blogs to routes and navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,17 @@
-import './Navbar.css';
+import { Link, BrowserRouter } from "react-router-dom";
 
+import './Navbar.css';
 
 export default function Navbar() {
   return (
-    <h1>here</h1>
+    <div>
+      <BrowserRouter>
+        <Link to="/about" className = "nav-link">About</Link>
+        <Link to="/blog" className = "nav-link">Blog</Link>
+        <Link to="/contact" className = "nav-link">Contact</Link>
+        <Link to="/projects" className = "nav-link">Projects</Link>
+        <Link to="/" className = "nav-link">Home</Link>
+      </BrowserRouter>
+    </div>
   )
 }
