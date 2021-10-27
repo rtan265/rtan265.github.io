1ab83f6fae424c0710948a126c51a24d13b4e3a0
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-10-27 16:02:25 +1300

    add return to routes.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Routes />
     </div>
   );
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,21 @@
 import { Route, Switch } from "react-router";
 
 export default function Routes() {
-  <Switch>
-    <Route exact path = "/">
-    </Route>
-    <Route path = "/about">
-    </Route>
-    <Route path = "/contact">
-    </Route>
-    <Route path = "/projects">
-    </Route>
-    <Route path = "/">
-    </Route>
-    <Route>
-        {/* insert 404 page here */}
-    </Route>
-  </Switch>
+  return (
+    <Switch>
+      <Route exact path = "/">
+      </Route>
+      <Route path = "/about">
+      </Route>
+      <Route path = "/contact">
+      </Route>
+      <Route path = "/projects">
+      </Route>
+      <Route path = "/">
+      </Route>
+      <Route>
+          {/* insert 404 page here */}
+      </Route>
+    </Switch>
+  )
 }
