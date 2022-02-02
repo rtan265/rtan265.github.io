89eb6a04a662afd13f5c6dae3fbea3dd034ae313
Ryan Tan <rtan265@aucklanduni.ac.nz>
2022-02-03 02:44:02 +1300

    code formatting and spacing between social icons

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -7,21 +7,26 @@ export default function About() {
   return (
     <div>
         <h1 className="about-page-header">Hi! I'm Ryan Tan.</h1>
-        <p className="about-page-body">I'm a 2021 graduate from the University of Auckland, who will soon be starting as an associate solutions architect at Amazon Web Services in Auckland. I built this website as a platform for me to experiment different technical ideas, as well as share my personal thoughts on certain topics.</p>
-          
-        <div className="about-page-socials">
-          <a href="http://www.linkedin.com/in/rtan18">
-            <FontAwesomeIcon icon={ faLinkedin } />
+        <p className="about-page-body">
+          I'm a 2021 graduate from the University of Auckland, who will soon be starting as an 
+          associate solutions architect at Amazon Web Services in Auckland. I built this website as a 
+          platform for me to experiment different technical ideas, as well as share my personal thoughts on certain topics.
+        </p>
+
+        <>
+          <a className="about-page-socials" href="http://www.linkedin.com/in/rtan18">
+            <FontAwesomeIcon size = "3x" icon = { faLinkedin } />
           </a> 
           
-          <a href="http://www.medium.com/@rtan265">
-            <FontAwesomeIcon icon={ faMedium } />
+          <a className="about-page-socials" href="http://www.medium.com/@rtan265">
+            <FontAwesomeIcon size = "3x" icon = { faMedium } />
           </a>
 
-          <a href="http://www.github.com/rtan265">
-            <FontAwesomeIcon icon={ faGithub } />
+          <a className="about-page-socials" href="http://www.github.com/rtan265">
+            <FontAwesomeIcon size = "3x" icon = { faGithub } />
           </a>
-        </div>
+        </>
+
     </div>
   )
 }
