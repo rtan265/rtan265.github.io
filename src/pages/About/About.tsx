dfb7dad56a684c3101d69ecc221b22a7beb75760
Ryan Tan <rtan265@aucklanduni.ac.nz>
2022-02-03 02:38:12 +1300

    add social fontawesome icons

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faMedium, faGithub } from "@fortawesome/free-brands-svg-icons";
+
 import "./About.css";
 
 export default function About() {
@@ -5,7 +8,20 @@ export default function About() {
     <div>
         <h1 className="about-page-header">Hi! I'm Ryan Tan.</h1>
         <p className="about-page-body">I'm a 2021 graduate from the University of Auckland, who will soon be starting as an associate solutions architect at Amazon Web Services in Auckland. I built this website as a platform for me to experiment different technical ideas, as well as share my personal thoughts on certain topics.</p>
-        <a href="http://www.linkedin.com/in/rtan18">LinkedIn</a> <a href="http://www.medium.com/@rtan265">Medium</a> <a href="http://www.github.com/rtan265">Github</a>
+          
+        <div className="about-page-socials">
+          <a href="http://www.linkedin.com/in/rtan18">
+            <FontAwesomeIcon icon={ faLinkedin } />
+          </a> 
+          
+          <a href="http://www.medium.com/@rtan265">
+            <FontAwesomeIcon icon={ faMedium } />
+          </a>
+
+          <a href="http://www.github.com/rtan265">
+            <FontAwesomeIcon icon={ faGithub } />
+          </a>
+        </div>
     </div>
   )
 }
