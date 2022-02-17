3988ed35ce7eb19f1ce234303140e21bd65c6f92
Ryan Tan <rtan265@aucklanduni.ac.nz>
2022-02-17 23:10:07 +1300

    Move information from About page to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,13 +3,18 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <div className = "container-ryan-photo">
+    <>
       <div className = "home-div">
         <img alt = "Ryan" className = "ryan-photo" src = { RyanProfilePicture }/>
         <p className = "ryan-intro-text">
           Hi! I'm Ryan Tan.
+          <h4>
+            I'm a 2021 graduate from the University of Auckland, who will soon be starting as an 
+            associate solutions architect at Amazon Web Services in Auckland. I built this website as a 
+            platform for me to experiment different technical ideas, as well as share my personal thoughts on certain topics.
+          </h4>
         </p>
       </div> 
-    </div>
+    </>
   )
 }
